1afc0718bb2c179401eb39a2075d770bf2044b32
aprilyanprayoga <aprilyanprayoga@gmail.com>
2026-09-14 15:37:12 +0700

    fix: tambah cors middleware untuk flutter web

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import categoryRoutes from "./routes/category.routes";
 import postRoutes from "./routes/post.routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
